9e8d0e578aead69f81f6baa4fbeff3648c656af0
bob44240 <bob@frieden.org>
2019-04-23 12:55:05 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+//import {HotTableModule} from "@handsontable/angular";
+
+import { AppComponent } from './app.component';
+import { MatDesignComponent } from './components/mat-design/mat-design.component';
+//import { HandsOnComponent } from './components/hands-on/hands-on.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MatDesignComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
